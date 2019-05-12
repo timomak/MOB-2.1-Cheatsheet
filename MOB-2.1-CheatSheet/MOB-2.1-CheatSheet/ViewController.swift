8371cb8219758d739802ba918a254e9190c6ffef
timo <tmakhlay2@gmail.com>
2019-05-12 00:00:20 -0700

    [ADD] Added xcode project

---
+++ MOB-2.1-CheatSheet/MOB-2.1-CheatSheet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MOB-2.1-CheatSheet
+//
+//  Created by timofey makhlay on 5/11/19.
+//  Copyright © 2019 Timofey Makhlay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MOB-2.1-CheatSheet/MOB-2.1-CheatSheetTests/MOB_2_1_CheatSheetTests.swift
@@ -0,0 +1,34 @@
+//
+//  MOB_2_1_CheatSheetTests.swift
+//  MOB-2.1-CheatSheetTests
+//
+//  Created by timofey makhlay on 5/11/19.
+//  Copyright © 2019 Timofey Makhlay. All rights reserved.
+//
+
+import XCTest
+@testable import MOB_2_1_CheatSheet
+
+class MOB_2_1_CheatSheetTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
